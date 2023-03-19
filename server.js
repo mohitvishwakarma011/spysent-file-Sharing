92f85765ff414b88f96ec965dd19b7e853a3fadc
Mohit Vishwakarma <mohitvishwakarma681@gmail.com>
2023-03-19 09:34:40 +0530

    added cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require('path');
+const cors= require('cors');
 
 const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
@@ -10,6 +11,13 @@ app.use(express.json());
 const connectDB = require("./config/db");
 connectDB();
 
+
+//CORS
+ const corsoption={
+    origin:process.env.ALLOWED_CLIENTS.split(',')
+ }
+ app.use(cors(corsoption));
+ 
 //Template engine  
 app.set('views',path.join(__dirname,'/views'))
 app.set('view engine','ejs');
