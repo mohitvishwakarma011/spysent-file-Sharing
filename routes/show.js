3800b8490e2785579bbe4e3fa10da5e1df90e418
Mohit Vishwakarma <mohitvishwakarma681@gmail.com>
2024-02-18 02:19:44 +0530

    download error fixed

--- routes/show.js
+++ routes/show.js
@@ -3,22 +3,29 @@ const router = require("express").Router();
 const File = require("../models/file");
 
 router.get("/:uuid", async (req, res) => {
-  try {
-    const file = await File.findOne({ uuid: req.params.uuid });
-    console.log(file);
-    if (!file) {
-      return res.render("download", { error: "Link has been expired!" });
+    try{
+
+      const file = await File.findOne({ uuid: req.params.uuid });
+
+      if (!file) {
+        return res.render("download", {
+          error: "Link has expired or does not exist.",
+        });
+      }
+
+      return res.render("download", {
+        uuid: file.uuid,
+        fileName: file.filename,
+        fileSize: file.size,
+        downloadLink: `${process.env.APP_BASE_URL}/files/download/${file.uuid}`,
+      });
+      
+    }catch{
+
+      console.error("Error fetching file:", err);
+      return res.render("download", { error: "something went wrong" });
     }
-
-    return res.render("download", {
-      uuid: file.uuid,
-      fileName: file.filename,
-      fileSize: file.size,
-      downloadLink: `${process.env.APP_BASE_URL}/files/download/${file.uuid}`,
+      
     });
-  } catch (err) {
-    return res.render("download", { error: "something went wrong" });
-  }
-});
-
+    
 module.exports = router;
